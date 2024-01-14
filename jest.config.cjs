44fd36d519f2e06e56a0843a974cd18b5a94a556
Alex Barbero <barrufat@hotmail.com>
2024-01-14 11:54:00 +0100

    Add connectToDataBase & configurations

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
+  setupFilesAfterEnv: ["./src/setupTest.ts"],
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
+  resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!src/index.ts",
+    "!src/server/app.ts",
+    "!src/database/index.ts",
+    "!src/setupTest.ts",
+    "!**/node_modules/**",
+  ],
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,11 @@
-// La cadena de middlewares 📎
+import "dotenv/config";
+import express from "express";
+import app from "./app.js";
+import pingRouter from "../ping/router/PingRouter.js";
+import morgan from "morgan";
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/", pingRouter);
